5e44dabe6ea520aeb62fb33ee5c566a03dd47a39
Ahmet Can Dogumlubay <101129959+ahmetcandogumlubay@users.noreply.github.com>
2022-03-07 19:53:41 +0300

    Delete pyth_1_exercise directory

--- pyth_1_exercise/exercise_1.6.py
+++
@@ -1,8 +0,0 @@
-A = float(input("Enter the initial money: "))
-P = float(input("Enter the banks interest rate: "))
-n = float(input("Enter the year: "))
-
-n_a_o_m = A*(1 + P/100)**n #new_amount_of_money
-gain = n_a_o_m - A
-
-print("This bank gives %.1f euros in %d years with %.1f%% interest from %.1f euros." %(gain,n,P,A))
